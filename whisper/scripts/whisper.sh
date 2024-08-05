dc2f83f6c44fdcc19e6873419b0466d8b675d017
Varanasi Stalin <varanasi.stalin@gmail.com>
2024-08-05 20:48:22 +0200

    removed whisper

--- whisper/scripts/whisper.sh
+++
@@ -1,5 +0,0 @@
-# bash /home/svaranasi/run_docker.sh --mem 150G  -p V100-32GB bash scripts/whisper.sh
-python=/netscratch/butt/miniconda3/envs/aiden_whisper/bin/python
-echo "In shell script"
-
-$python whisper_try.py /netscratch/butt/aiden/data "base"
